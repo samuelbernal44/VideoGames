6a536bba8e9d2e0b585d7487d04137adc2f967f7
samuelbernal44 <samuelbernal44@gmail.com>
2023-08-14 20:56:56 -0500

    Background image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,24 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import { LandingPage, HomePage, Detail, Form, SearchResults } from './views';
 
 function App() {
   const { pathname } = useLocation();
 
+  useEffect(() => {
+    if (pathname === '/') {
+      document.body.classList.add('landing-page');
+    } else {
+      document.body.classList.remove('landing-page');
+    }
+  }, [pathname]);
+
   return (
     <>
       {pathname !== '/' && <NavBar />}
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<LandingPage class="landing-page" />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/home/name" element={<SearchResults />} />
         <Route path="/detail/:id" element={<Detail />} />
