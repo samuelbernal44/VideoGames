6213ad5cee8d30ac2ea68f9b6ab28af7088f7472
samuelbernal44 <samuelbernal44@gmail.com>
2023-08-15 01:30:43 -0500

    Solución paginado, filtros y ordenamientos

--- client/src/views/SearchResults/SearchResults.jsx
+++ client/src/views/SearchResults/SearchResults.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { connect } from 'react-redux';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import GameCard from '../../components/GameCard/GameCard';
 import {
   ContainerList,
@@ -21,6 +21,29 @@ function SearchResults({ searchResults }) {
   const [sortDirection, setSortDirection] = useState('asc');
 
   const games = searchResults;
+
+  useEffect(() => {
+    const storedGenre = localStorage.getItem('selectedGenre');
+    if (storedGenre) {
+      setSelectedGenre(storedGenre);
+    }
+    const storedOrigin = localStorage.getItem('selectedOrigin');
+    if (storedOrigin) {
+      setSelectedOrigin(storedOrigin);
+    }
+    const storedSortCriteria = localStorage.getItem('sortCriteria');
+    if (storedSortCriteria) {
+      setSortCriteria(storedSortCriteria);
+    }
+    const storedSortDirection = localStorage.getItem('sortDirection');
+    if (storedSortDirection) {
+      setSortDirection(storedSortDirection);
+    }
+    const storedPage = localStorage.getItem('currentPage');
+    if (storedPage) {
+      setCurrentPage(Number(storedPage));
+    }
+  }, []);
 
   // Filtrar juegos por género
   let filteredGames;
@@ -62,33 +85,42 @@ function SearchResults({ searchResults }) {
   // Manejadores para cambiar de página
   const handleNextPage = () => {
     if (currentPage < totalPages - 1) {
-      setCurrentPage(currentPage + 1);
+      const newPage = currentPage + 1;
+      setCurrentPage(newPage);
+      localStorage.setItem('currentPage', newPage);
     }
   };
 
   const handlePrevPage = () => {
     if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
+      const newPage = currentPage - 1;
+      setCurrentPage(newPage);
+      localStorage.setItem('currentPage', newPage);
     }
   };
 
   // Controladores para actualizar el género, el origen y el criterio de ordenamiento seleccionados
   const handleFilterChange = (genre) => {
     setSelectedGenre(genre);
+    localStorage.setItem('selectedGenre', genre);
   };
   const handleOriginChange = (origin) => {
-    // console.log(origin);
     setSelectedOrigin(origin);
+    localStorage.setItem('selectedOrigin', origin);
   };
 
   const handleNameSortChange = (event) => {
     setSortCriteria('name');
     setSortDirection(event);
+    localStorage.setItem('sortCriteria', 'name');
+    localStorage.setItem('sortDirection', event);
   };
 
   const handleRatingSortChange = (event) => {
     setSortCriteria('rating');
     setSortDirection(event);
+    localStorage.setItem('sortCriteria', 'rating');
+    localStorage.setItem('sortDirection', event);
   };
 
   return (
@@ -98,6 +130,10 @@ function SearchResults({ searchResults }) {
         onOriginChange={handleOriginChange}
         onSortName={handleNameSortChange}
         onSortRating={handleRatingSortChange}
+        selectedGenre={selectedGenre}
+        selectedOrigin={selectedOrigin}
+        sortCriteria={sortCriteria}
+        sortDirection={sortDirection}
       />
       <ContainerList>
         {currentGames.map((game) => (
